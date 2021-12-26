e9d1bc3c6c3d8f03baa74eca9f3084bb34173ba3
Mustafa Can Palaz <mustafacanpalaz@gmail.com>
2021-12-26 15:06:05 +0300

    Add args, update twitter selector and refactoring

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# Archis Social Media Bot #
+# Social Media Bot
 
-`npm install`
+Gets text content of a given Facebook or Twitter link with [puppeteer](https://pptr.dev/) Headless Chrome Node.js API.
 
-`node index.js`
+[curl](https://curl.se/) or [HTTPie](https://httpie.io/) can be easier and faster to get this kind of data. This is just an example of puppeteer application.
 
+**Install packages**
+
+`yarn install`
+
+**Run the script with a public Facebook/Twitter content**
+
+`node index.js [URL]`
--- index.js
+++ index.js
@@ -1,75 +1,77 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 // Timeout limit for the page.waitForSelector function
 // See on log as 'Find Data'
 const TIMEOUT = 5000;
 
-// We will get this input from the user
-// Test URL: "https://twitter.com/elonmusk/status/1294917318405836802", Expected Output: "Post Content: We must pass The Great Filter"
-// Test URL: "https://www.facebook.com/TheElonmusk/posts/3692368240776048", Expected Output: "One hundred years of American Innovation."
-const input = "https://www.facebook.com/TheElonmusk/posts/3692368240776048";
-
-const websites = {
-    facebook: {
-        selector: "[data-testid='post_message']"
-    },
-    twitter: {
-        selector: "[data-testid='tweet'] ~ div div:first-child div:first-child"
-    }
-};
-
-const getWebsite = url => {
-    if (url.includes("facebook.com")) {
-        return "facebook";
-    } else if (url.includes("twitter.com")) {
-        return "twitter";
-    } else {
-        return null;
-    }
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log("Error");
+  process.exit(1);
 }
 
-const getPostContent = async () => {
-    const website = websites[getWebsite(input)];
-    if(!website) {
-        return false;
-    }
+const inputUrl = args[0];
+// const inputUrl = "https://twitter.com/elonmusk/status/1294917318405836802"; // We must pass The Great Filter
+// const inputUrl = "https://www.facebook.com/TheElonmusk/posts/3692368240776048"; // One hundred years of American Innovation.
 
-    const startTime1 = new Date();
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(input);
-    end(startTime1, "Open Page");
-
-    try {
-        const startTime2 = new Date();
-        const element = await page.waitForSelector(website.selector, {timeout: TIMEOUT});
-        end(startTime2, "Find Data");
-        const text = await (await element.getProperty('textContent')).jsonValue();
-        return text;
-    } catch (e) {
-        if (e instanceof puppeteer.errors.TimeoutError) {
-            return false;
-        }
-    }
-
-    await browser.close();
+const querySelectors = {
+  facebook: {
+    selector: "[data-testid='post_message']",
+  },
+  twitter: {
+    selector: "[data-testid='tweet'] div:nth-child(3) span",
+  },
 };
 
+const getApplication = (url) => {
+  if (url.includes("facebook.com")) {
+    return "facebook";
+  } else if (url.includes("twitter.com")) {
+    return "twitter";
+  } else {
+    return null;
+  }
+};
+
+const getPostContent = async () => {
+  const app = querySelectors[getApplication(inputUrl)];
+  if (!app) {
+    return false;
+  }
+
+  const startTime1 = new Date();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(inputUrl);
+  logTiming(startTime1, "Open Page");
+
+  try {
+    const element = await page.waitForSelector(app.selector, {
+      timeout: TIMEOUT,
+    });
+    const text = await (await element.getProperty("textContent")).jsonValue();
+    return text;
+  } catch (e) {
+    if (e instanceof puppeteer.errors.TimeoutError) {
+      return false;
+    }
+  }
+
+  await browser.close();
+};
 
 (async () => {
-    const startTime = new Date();
-    const text = await getPostContent();
-    end(startTime);
+  const startTime = new Date();
+  const text = await getPostContent();
+  logTiming(startTime);
 
-    text ? 
-        console.log(`Post Content: ${text}`) :
-        console.log("Not found...");
+  text ? console.log(`Post Content: ${text}`) : console.log("Not found...");
+  process.exit(1);
 })();
 
-
-function end(startTime, message="Total Time") {
-    endTime = new Date();
-    const timeDiff = endTime - startTime;
-    const seconds = Math.round(timeDiff);
-    console.log(`${message}: ${seconds} ms`);
+function logTiming(startTime, message = "Total Time") {
+  endTime = new Date();
+  const timeDiff = endTime - startTime;
+  const seconds = Math.round(timeDiff);
+  console.log(`${message}: ${seconds} ms`);
 }
